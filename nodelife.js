f62a298e62ea8464b7548af352ea29ef2f1f5d19
Hraefn Grey <snessnet@gmail.com>
2020-11-01 17:04:47 -0800

    Added nodelife

---
+++ nodelife.js
@@ -0,0 +1,52 @@
+const width = 30
+const height = 30:
+let gen = 0;
+
+function update(a) {
+    b = new Array(width * height).fill(0)
+    for (y = 0; y < height; y++) {
+        yyy = y * width;
+        ym1 = ((y + height - 1) % height) * width
+        yp1 = ((y + 1) % height) * width
+        xm1 = width - 1
+        for (x = 0; x < width; x++) {
+            xp1 = (x + 1) % width
+            neighbours = (
+                a[xm1 + ym1] + a[xm1 + yyy] + a[xm1 + yp1] +
+                    a[x + ym1] + a[x + yp1] +
+                    a[xp1 + ym1] + a[xp1 + yyy] + a[xp1 + yp1])
+            if (neighbours == 3 || (neighbours == 2 && a[yyy+x])) {
+                b[yyy+x] = 1
+            } else {
+                b[yyy+x] = 0
+            }
+            xm1 = x
+        }
+    }
+    return b
+}
+
+function print(a) {
+    console.clear()
+    for (y = 0; y < height; y++) {
+        yyy = y * width;
+        process.stdout.write("|")
+        for (x = 0; x < width; x++) {
+            c = a[yyy+x] == 0 ? " " : "+"
+            process.stdout.write(c)
+        }
+        process.stdout.write("|\n")
+    }
+    console.log("gen " + gen)
+}
+
+curr = new Array(width * height).fill(0);
+curr[30] = 1
+curr[31] = 1
+curr[32] = 1
+while (true) {
+    print(curr)
+    next = update(curr)
+    curr = next
+    gen++
+}
